c3d590d8d91dae71e91560aa4793c3e4aa172a05
bobJiang <374123140@qq.com>
2024-01-04 18:00:51 +0800

    开闭原则

--- 01/3-开闭原则.go
+++ 01/3-开闭原则.go
@@ -16,11 +16,32 @@ func (s *SaveBanker) DoBusiness() {
 	fmt.Println("存款业务")
 }
 
-// 新加功能直接扩展
+// TransferBanker （新加功能直接扩展）
 type TransferBanker struct {
 	AbstractBanker
 }
 
-func (s *TransferBanker) DoBusiness() {
+type ShareBanker struct {
+	AbstractBanker
+}
+
+func (t *TransferBanker) DoBusiness() {
 	fmt.Println("转账业务")
+}
+
+func (s *ShareBanker) DoBusiness() {
+	fmt.Println("股票业务")
+}
+
+// bankBusiness 该模式也可以抽象出一个架构层
+func bankBusiness(banker AbstractBanker) {
+	//利用多态，父指针能够调到子对象的方法
+	banker.DoBusiness()
+}
+
+func main() {
+	//股票业务
+	bankBusiness(&ShareBanker{})
+	//转账业务
+	bankBusiness(&TransferBanker{})
 }
