1697a0a023c0a61c41d0d6ef329ca9fcdbc40027
bobJiang <374123140@qq.com>
2025-01-09 16:27:08 +0800

    add 简单工程测试

--- creational-pattern/factory/test/factory_test.go
+++ creational-pattern/factory/test/factory_test.go
@@ -15,3 +15,13 @@ func TestV0(t *testing.T) {
 		fmt.Println("Config loaded:", config)
 	}
 }
+
+func TestV1(t *testing.T) {
+	source := &factory.RuleConfigSource{}
+	load, err := source.Load2("./rule.json")
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Config loaded:", load)
+	}
+}
